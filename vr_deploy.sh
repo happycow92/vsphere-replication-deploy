c6a3707f82da072f2801537e63894a363d53af42
Suhas G <virtuallypeculiar@gmail.com>
2017-06-16 04:17:28 +0530

    Update vr_deploy.sh

--- vr_deploy.sh
+++ vr_deploy.sh
@@ -21,21 +21,31 @@ else
 
 # Downloading OVF tool from repository
 
-	cd /root
+        cd /root
 
         if ping -q -c 1 -W 1 8.8.8.8 &> /dev/null;
         then
-        	printf "Downloading OVF Tool\n"
-        	wget https://www.dropbox.com/s/wihvc41gzfl7ca8/VMware-ovftool-4.2.0-4586971-lin.x86_64.bundle?dl=0 -O /root/VMware-ovftool-4.2.0-4586971-lin.x86_64.bundle -q
-        	echo
+			ovftool --help &> out.txt
+			var=$(cat out.txt  | awk '{print $3$4$5}')
+			if [ "$var" == "Nosuchfile" ]
+			then
+				printf "\nOVF Tool Not Present\n"
+				printf "Downloading OVF Tool\n"
+				wget https://www.dropbox.com/s/wihvc41gzfl7ca8/VMware-ovftool-4.2.0-4586971-lin.x86_64.bundle?dl=0 -O /root/VMware-ovftool-4.2.0-4586971-lin.x86_64.bundle -q
+                echo
+				
+				# Install OVF tool
+				printf "Installing OVF Tool\n\n"
+				echo -ne '\n' | sudo /bin/sh VMware-ovftool-4.2.0-4586971-lin.x86_64.bundle --eulas-agreed
+			else
+				printf "\nOVF Tool exists. Not downloading again\n"
+			fi
         else
-        	printf "No network connection. Download OVF tool manually and install it on this appliance"
-        	exit 1
+			printf "No network connection. Download OVF tool manually and install it on this appliance"
+			exit 1
         fi
 
-# Install OVF tool
-        printf "Installing OVF Tool\n\n"
-        echo -ne '\n' | sudo /bin/sh VMware-ovftool-4.2.0-4586971-lin.x86_64.bundle --eulas-agreed
+
 
 # Mounting vSphere Replication ISO
         printf "\n\nCreating Mount Point for ISO\n"
@@ -59,8 +69,8 @@ else
         echo #New Line
         read -p "Enter the IP of the vCenter Server in which the appliance should be deployed: " vcenter_address
         echo #New Line
-	read -p "Enter the IP of the ESXi host where this appliance should run on: "  esxi_address
-	echo #New Line
+		read -p "Enter the IP of the ESXi host where this appliance should run on: "  esxi_address
+		echo #New Line
         read -p "Enter the SSO username for the vCenter server this vR will be deployed on: " sso_user
         echo #New Line
         read -p "Enter the password for this SSO user: " sso_password
@@ -72,7 +82,8 @@ else
         printf "\nPerforming Deployment\n\n"
         ovftool --acceptAllEulas -ds="$datastore_name" -n="$display_name" --net:"Management Network"="$network_group" --prop:"password"=$root_password --prop:"ntpserver"="$ntp_address" --prop:"vami.ip0.vSphere_Replication_Appliance"="$replication_address" --vService:installation=com.vmware.vim.vsm:extension_vservice /mnt/iso/bin/vSphere_Replication_OVF10.ovf vi://"$sso_user":$sso_password@$vcenter_address/?ip=$esxi_address
 		
-	printf "\n\nDeployment Complete\nExiting Script\n\n"
+		printf "\n\nDeployment Complete\nExiting Script\n\n"
 fi
 
 
+rm -f out.txt
